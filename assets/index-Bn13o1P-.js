ac950a790e356d2d60031b5e0cd0c8c9b48f19bd
jenkins-admin <xxxx@gmail.com>
2025-06-02 19:46:27 +0900

    Build #5

---
+++ assets/index-Bn13o1P-.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-hmQRhdCg.js";import"./vendor-Cc1dUZzU.js";const s=()=>t.jsx("div",{children:t.jsx("h1",{children:"About"})});export{s as default};
---
+++ assets/index-T6CLkuhM.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-hmQRhdCg.js";import"./vendor-Cc1dUZzU.js";const n=()=>t.jsx("div",{children:t.jsx("h1",{children:"Contact"})});export{n as default};
