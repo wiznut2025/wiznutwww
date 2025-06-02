a992246785bacc6b879dd808a1fd5c52011a21c3
jenkins-admin <xxxx@gmail.com>
2025-06-02 20:09:41 +0900

    Build #7

---
+++ assets/index-Svnxsj49.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DuFb8MrM.js";import"./vendor-Cc1dUZzU.js";const s=()=>t.jsx("div",{children:t.jsx("h1",{children:"About"})});export{s as default};
---
+++ assets/index-Y45ZLk2M.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DuFb8MrM.js";import"./vendor-Cc1dUZzU.js";const n=()=>t.jsx("div",{children:t.jsx("h1",{children:"Contact"})});export{n as default};
