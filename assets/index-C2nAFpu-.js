cabdfa8277b35a224a4f89c16f1467263b715217
jenkins-admin <xxxx@gmail.com>
2025-06-02 19:57:41 +0900

    Build #6

---
+++ assets/index-C2nAFpu-.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-B-s6E8FN.js";import"./vendor-Cc1dUZzU.js";const s=()=>t.jsx("div",{children:t.jsx("h1",{children:"About"})});export{s as default};
---
+++ assets/index-mOhIDdxO.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-B-s6E8FN.js";import"./vendor-Cc1dUZzU.js";const n=()=>t.jsx("div",{children:t.jsx("h1",{children:"Contact"})});export{n as default};
