09ec42e210c2c343c46db0b22bb44ed53afa7516
jenkins-admin <xxxx@gmail.com>
2025-06-12 14:43:53 +0900

    Build #8

---
+++ assets/index-COI7_htZ.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CQF4DCK7.js";import"./vendor-Cc1dUZzU.js";const n=()=>t.jsx("div",{children:t.jsx("h1",{children:"Contact"})});export{n as default};
---
+++ assets/index-LwFEcd3C.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CQF4DCK7.js";import"./vendor-Cc1dUZzU.js";const s=()=>t.jsx("div",{children:t.jsx("h1",{children:"About"})});export{s as default};
