a4f18cf255f152c819a8b38767d1fa27a725b7a4
jenkins-admin <xxxx@gmail.com>
2026-04-14 13:46:49 +0900

    Build #11

---
+++ assets/index-98FE06_i.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-sbYr4i8A.js";import"./vendor-BJPtp0bd.js";const s=()=>t.jsx("div",{children:t.jsx("h1",{children:"About"})});export{s as default};
---
+++ assets/index-CaJWD6Ny.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-sbYr4i8A.js";import"./vendor-BJPtp0bd.js";const n=()=>t.jsx("div",{children:t.jsx("h1",{children:"Contact"})});export{n as default};
