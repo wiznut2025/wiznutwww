4d8ac422bb4d1d52dde87ab7b5af4d3e01a4ba1c
jenkins-admin <xxxx@gmail.com>
2026-06-25 21:23:58 +0900

    Build #12

---
+++ assets/index-C2U5wkND.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BY_3pseI.js";import"./vendor-BJPtp0bd.js";const n=()=>t.jsx("div",{children:t.jsx("h1",{children:"Contact"})});export{n as default};
---
+++ assets/index-vORZEcB8.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BY_3pseI.js";import"./vendor-BJPtp0bd.js";const s=()=>t.jsx("div",{children:t.jsx("h1",{children:"About"})});export{s as default};
